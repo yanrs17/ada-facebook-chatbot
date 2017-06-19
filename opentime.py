8aae48c7bd00e66d0a4d5d51ae6c3bb1c0ba4ef4
Zhaocong Yuan <justin.yuan@mail.utoronto.ca>
2017-06-19 11:17:06 -0400

    worked at local

--- opentime.py
+++ opentime.py
@@ -70,14 +70,14 @@ LIST_OF_LIBRARIES = [
 ];
 
 def getOpentime(second):
-	if second == "ba":
-        #return "24/7/365, 程序员不用休息哒（¯﹃¯）<br/>"
-        return "Everyday!!! Computer scientist do not need a break~（¯﹃¯）<br/>"
+    if second == "ba":
+        #return "24/7/365, 程序员不用休息哒（¯﹃¯"
+        return "Everyday!!! Computer scientist do not need a break"
 
     # If the library entered is not in the DICT, then ask the user to enter library again
     if second not in DICT_OF_LIBRARIES:
         #return "抱歉哦,小助手找不到你输入的图书馆,你是不是要找以下的图书馆呢?>_<<br/>" + getSuggestedLibraries() + "请输入图书馆简称哦 (e.g.: library kf) _(:3 」∠)_ "
-        return "Sorry, the library you entered is not valid.<br/> Please re-enter the  library you are looking for using the abbreviation. (i.e. lib rb)<br/>" + getSuggestedLibraries()
+        return "Sorry, the library you entered is not valid.<br/> Please re-enter the  library you are looking for using the abbreviation. (i.e. lib rb)\n" + getSuggestedLibraries()
     # If the library is in the DICT, then parser the web for information
     else:
         lib_dic = getInfo()
@@ -86,9 +86,9 @@ def getOpentime(second):
         url = lib_dic[name][1].encode(encoding="utf-8", errors="strict")
 
         if url == "Please contact library for hours.":
-            return " Open Time for {}: <br/>{}.<br/>To look for open time for future, please check here:<br/>{}".format(name, hour, url)
+            return " Open Time for {}: \n{}.\nTo look for open time for future, please check here:\n{}".format(name, hour, url)
         else:
-            return " Open Time for {}: <br/>{}<br/>To look for open time for future, please check here:<br/><a href =\"{}\">{}</a>".format(name, hour, url, url)
+            return " Open Time for {}: \n{}<br/>To look for open time for future, please check here:\n<a href =\"{}\">{}</a>".format(name, hour, url, url)
 
 #==========helper function==========
 def getInfo():
@@ -111,4 +111,4 @@ def getInfo():
     return dic
 
 def getSuggestedLibraries():
-    return '<br/>'.join(LIST_OF_LIBRARIES)
+    return '\n'.join(LIST_OF_LIBRARIES)
